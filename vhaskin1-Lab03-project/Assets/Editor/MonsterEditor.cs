acb5e4b770367703282a16832dc94e8cf3709eab
sandrockman <svc-student@RC37815.student.directory.jccc>
2015-09-02 09:53:53 -0500

    added project

---
+++ vhaskin1-Lab03-project/Assets/Editor/MonsterEditor.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Monster))]
+public class MonsterEditor : Editor {
+
+	Monster monsterScript;
+	bool visibleRange = false;
+	bool transitionRange = false;
+	bool spawnTypes = false;
+
+	void Awake()
+	{
+		monsterScript = (Monster)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		//Display a foldout, with the default value stored in visibleRange & the label "Visibility Settings"
+		//As the foldout is manipulated, save changes into visibleRange
+		visibleRange = EditorGUILayout.Foldout (visibleRange, "Visibility Settings");
+		if (visibleRange) 
+		{
+			//Increase the indent level by one
+			EditorGUI.indentLevel++;
+			//the variable VariableVisibletime on the monsterScript should equal the setting the user picked for the toggle
+			//the toggle should display the variable VariableVisibleTime
+			monsterScript.VariableVisibleTime = EditorGUILayout.Toggle("Vaviable Visible Time?",
+			                                                           monsterScript.VariableVisibleTime);
+			if(monsterScript.VariableVisibleTime)
+			{
+				EditorGUILayout.LabelField("Maximum - Minimum Time");
+				EditorGUILayout.BeginHorizontal();
+				//Since I manually created the label above, I am using an overload of the float field so 
+				//no default label is displayed immediately before it.
+				monsterScript.VisibleTimeMax = EditorGUILayout.FloatField(monsterScript.VisibleTimeMax);
+				monsterScript.VisibleTimeMin = EditorGUILayout.FloatField(monsterScript.VisibleTimeMin);
+				EditorGUILayout.EndHorizontal();
+			}
+			else
+			{
+				EditorGUILayout.BeginHorizontal();
+				EditorGUILayout.PrefixLabel("VisibleTime:");
+				//Again, I manually created a prefix label to display so I am overloading the float field so no
+				//default label is shown
+				monsterScript.VisibleTimeMax = EditorGUILayout.FloatField (monsterScript.VisibleTimeMax);
+				EditorGUILayout.EndHorizontal();
+			}
+			//Reduce the indent level by one
+			EditorGUI.indentLevel--;
+		}
+		EditorGUILayout.Space ();
+
+		//same foldout, but for transitionRange
+		transitionRange = EditorGUILayout.Foldout (transitionRange, "Transition Settings");
+		if (transitionRange) 
+		{
+			//Increase the indent level by one
+			EditorGUI.indentLevel++;
+			//The variable VariableTransitionTime on the monsterScript should 
+			//Reduce the indent level by one
+			EditorGUI.indentLevel--;
+
+		}
+		EditorGUILayout.Space ();
+
+		//same foldout, but for spawnTypes
+		spawnTypes = EditorGUILayout.Foldout (spawnTypes, "Spawn Settings");
+
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ vhaskin1-Lab03-project/Assets/Scripts/Monster.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum MonsterType
+{
+	GHOST_BLUE,
+	GHOST_ORANGE,
+	GHOST_RED,
+	LENGTH
+}
+public class Monster : MonoBehaviour {
+	
+	float initialHeight;
+	
+	#region Visible Time
+	//Can this monster be visible for different lengths of time?
+	private bool variableVisibleTime = false;
+	public bool VariableVisibleTime
+	{
+		get { return variableVisibleTime; }
+		set { variableVisibleTime = value; }
+	}
+	
+	//How long the monster is visibsle for (max)
+	private float visibleTimeMax = 3;
+	public float VisibleTimeMax
+	{
+		get { return visibleTimeMax; }
+		set { visibleTimeMax = value; }
+	}
+	
+	//How long the monster is visible for (min) Only applies if Variable Visible Time is true
+	private float visibleTimeMin;
+	public float VisibleTimeMin
+	{
+		get { return visibleTimeMin; }
+		set { visibleTimeMin = value; }
+	}
+	#endregion
+	
+	#region Transition Time
+	private bool variableTransitionTime = false;
+	public bool VariableTransitionTime
+	{
+		get { return variableTransitionTime; }
+		set { variableTransitionTime = value; }
+	}
+	
+	private float transitionTimeMax = 2;
+	public float TransitionTimeMax
+	{
+		get { return transitionTimeMax; }
+		set { transitionTimeMax = value; }
+	}
+	
+	private float transitionTimeMin;
+	public float TransitionTimeMin
+	{
+		get { return transitionTimeMin; }
+		set { transitionTimeMin = value; }
+	}
+	#endregion
+	
+	
+	private int pointWorth;
+	public int PointWorth
+	{
+		get { return pointWorth; }
+		set { pointWorth = value; }
+	}
+	
+	//The types of monsters this monster can be
+	public MonsterType[] possibleTypes;
+	
+	//The type of monster this monster currently is
+	MonsterType myType;
+	
+	public bool disappearing;
+	public bool visible;
+}
+
